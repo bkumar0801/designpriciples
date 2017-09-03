da2929b055d458e55dd751b844aee8e23842f945
bijendrakumar <kbijen@thoughtworks.com>
2017-09-03 17:22:17 +0530

    Update Area Aggregator to use Area of different shape

--- src/main/problem/AreaAggregator.java
+++ src/main/problem/AreaAggregator.java
@@ -13,11 +13,7 @@ public class AreaAggregator {
     public int Sum() {
         int sum = 0;
         for (Shape shape: Shapes) {
-            if(shape.getClass().equals(Circle.class)) {
-                sum += Math.PI * Math.pow(((Circle) shape).getRadius(), 2);
-            } else if (shape.getClass().equals(Square.class)) {
-                sum += Math.pow(((Square) shape).getSide(), 2);
-            }
+           sum += shape.Area();
         }
         return sum;
     }
